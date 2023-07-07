cd91eab9b64505922dcb50792967b7639a387098
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-08 01:32:00 +0530

    schema validation using pydantic

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get('/')
 def root():
     return {'message':'running'}
 
 @app.post('/create_post')
-def create_post(payload : dict = Body(...)):
-    print(payload)
-    return{"new_post":f'created new post with title:{payload["title"]} and content:{payload["content"]}'}
+def create_post(new_post : Post):
+    print(new_post)
+    return{"data":"new post"}
