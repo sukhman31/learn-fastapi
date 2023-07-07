3035a1ea44f135ea313b4f0d5fadd50dcf7e4242
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-07 19:09:29 +0530

    add post route

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get('/')
-async def root():
+def root():
     return {'message':'running'}
+
+@app.post('/create_post')
+def create_post(payload : dict = Body(...)):
+    print(payload)
+    return{"new_post":f'created new post with title:{payload["title"]} and content:{payload["content"]}'}
