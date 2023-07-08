21ad4da80d406b167c2b757b9a5dd15b2c71ce49
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-09 02:39:24 +0530

    adding foreign key constraint to users and posts table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -11,7 +11,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
-
+    owner_id = Column(Integer,ForeignKey('users.id',ondelete='CASCADE'),nullable=False)
 
 class User(Base):
     __tablename__ = 'users'
