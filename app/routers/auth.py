c1283602ae117a3a0e3f73c6574ca2f14d8a5225
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-07-09 01:31:55 +0530

    update error codes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,10 +12,10 @@ router = APIRouter(
 def login(user_cred : OAuth2PasswordRequestForm=Depends(),db : Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == user_cred.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f'Invalid credentials')
     if not utils.verify(user_cred.password,user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail=f'Invalid credentials')
     # create token
     access_token = oauth2.create_access_token(data={'user_id':user.id})
